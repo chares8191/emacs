7d606256c33f15a78ba62322a3022c622a7bdb1f
Chares <chares8191@proton.me>
2025-10-25 14:17:37 -0400

    added frame transparancy

--- settings/init-frame-settings.el
+++ settings/init-frame-settings.el
@@ -1,4 +1,5 @@
-;; (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(font . "Space Mono 14"))
+(add-to-list 'default-frame-alist '(alpha-background . 84))
 
 (provide 'init-frame-settings)
