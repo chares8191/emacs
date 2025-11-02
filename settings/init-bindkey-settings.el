151f3b1be878d7d53720cefcea919608b7a63cf3
Chares <chares8191@proton.me>
2025-11-01 23:51:18 -0400

    Regexp search

--- settings/init-bindkey-settings.el
+++ settings/init-bindkey-settings.el
@@ -1,5 +1,10 @@
 ;; [.emacs.d/settings] init-bindkey-settings
 
+;; regexp search by default
+;; (global-set-key (kbd "C-s") 'isearch-forward-regexp)
+;; (global-set-key (kbd "C-r") 'isearch-backward-regexp)
+;; (global-set-key (kbd "M-%") 'query-replace-regexp)
+
 (global-set-key (kbd "s-<left>") #'beginning-of-line)
 (global-set-key (kbd "s-<right>") #'end-of-line)
 (global-set-key (kbd "s-<up>") #'beginning-of-buffer)
