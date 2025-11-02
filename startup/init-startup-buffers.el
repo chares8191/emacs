38c6692f018f5487e744407230a4b51b26e2bfd0
Chares <chares8191@proton.me>
2025-11-02 00:22:19 -0400

    startup-buffers

--- startup/init-startup-buffers.el
+++ startup/init-startup-buffers.el
@@ -1,9 +1,8 @@
 ;; startup/init-startup-buffers
 
-(find-file
- (expand-file-name ".xinitrc" (directory-file-name (getenv "HOME"))))
-(find-file
- (expand-file-name ".zshrc" (directory-file-name (getenv "HOME"))))
+
+(find-file (expand-file-name (getenv "ZSHENV_FILE")))
+(find-file (expand-file-name (getenv "ZSHRC_FILE")))
 (find-file (expand-file-name "init.el" user-emacs-directory))
 (find-file user-emacs-directory)
 
