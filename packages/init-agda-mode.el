e484bbfe5b1f82bafb17cacade6b530f83497975
Chares <chares8191@proton.me>
2026-02-15 23:18:20 -0500

    agda 2.8

--- packages/init-agda-mode.el
+++ packages/init-agda-mode.el
@@ -1,9 +1,10 @@
 ;; [.emacs.d/packages] init-agda-mode
 
 (let ((agda-file
-	   (car (ignore-errors (process-lines "agda-mode" "locate")))))
+       (car
+        (ignore-errors
+          (process-lines "agda" "--emacs-mode" "locate")))))
   (when (and agda-file (file-exists-p agda-file))
-	(load-file agda-file)))
+    (load-file agda-file)))
 
 (provide 'init-agda-mode)
-
