2f7999f21ce715b8334d289034c20e73fd45555c
Chares <chares8191@proton.me>
2026-02-22 16:44:39 -0500

    Adding Tibell's haskell-style-guide as submodule

--- packages/init-haskell-mode.el
+++ packages/init-haskell-mode.el
@@ -1,5 +1,13 @@
 ;; [.emacs.d/packages] init-haskell-mode
 
+;; https://github.com/tibbe/haskell-style-guide
+(add-to-list
+ 'load-path
+ (expand-file-name
+  (directory-file-name "haskell-style-guide")
+  (directory-file-name emacsdir/packages)))
+(require 'haskell-style)
+
 (use-package haskell-mode
   :straight t)
 
--- packages/init-mode-minder.el
+++ packages/init-mode-minder.el
@@ -1,11 +1,11 @@
 ;; [emacsdir/packages] :: init-mode-minder
 
+;; https://github.com/jdtsmith/mode-minder
 (add-to-list
  'load-path
  (expand-file-name
   (directory-file-name "mode-minder")
   (directory-file-name emacsdir/packages)))
-
 (require 'mode-minder)
 
 (provide 'init-mode-minder)
