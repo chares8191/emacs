2f098dccced9ecfa39b59dd5c93f6384de39e572
Chares <chares8191@proton.me>
2026-05-03 20:38:05 -0400

    debug transient

--- init-load-packages.el
+++ init-load-packages.el
@@ -18,7 +18,7 @@
 (require 'init-rust-mode)
 (require 'init-haskell-mode)
 (require 'init-agda-mode)
-(require 'init-lean4-mode)
+;; (require 'init-lean4-mode)
 (require 'init-yaml-mode)
 (require 'init-json-mode)
 
