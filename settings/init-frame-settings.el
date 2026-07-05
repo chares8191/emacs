891c1cf1f352bb2950a8565851a429f9d6b860ff
Chares <chares8191@proton.me>
2026-07-05 13:01:17 -0400

    Font change

--- settings/init-frame-settings.el
+++ settings/init-frame-settings.el
@@ -1,7 +1,7 @@
 ;; [emacsdir/settings] init-frame-settings
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(font . "Space Mono 14"))
+(add-to-list 'default-frame-alist '(font . "Iosevka 14"))
 (add-to-list 'default-frame-alist '(alpha-background . 84))
 
 (provide 'init-frame-settings)
