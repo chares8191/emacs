712bd883d996fe034482bb4bdaad2bd1a6fd4f2e
Chares <chares8191@proton.me>
2026-08-02 01:06:12 -0400

    Portability Fix 1/

--- packages/init-mode-minder.el
+++ packages/init-mode-minder.el
@@ -1,7 +1,14 @@
 ;; [emacsdir/packages] :: init-mode-minder
 
 ;; https://github.com/jdtsmith/mode-minder
-(add-to-list 'load-path (expand-file-name "mode-minder" emacsdir/packages))
-(require 'mode-minder)
+(defun mode-minder-path ()
+  (expand-file-name "mode-minder" emacsdir/packages))
+
+(let ((mode-minder-file (expand-file-name "mode-minder.el" (mode-minder-path))))
+  (if (file-readable-p mode-minder-file)
+      (progn
+        (add-to-list 'load-path (mode-minder-path))
+        (require 'mode-minder))
+    (message "[emacsdir/packages] init-mode-minder :: skipped")))
 
 (provide 'init-mode-minder)
